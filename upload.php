90a68dc809b2ce74d65a8cb8f5c7a18b40603329
Ali Iktider Sayam <aisayam.sayam@gmail.com>
2020-07-15 22:39:04 +0600

    first

---
+++ upload.php
@@ -0,0 +1,48 @@
+<?php
+$target_dir = "";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$checkFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+
+	  if($checkFileType != "txt") 
+	  {
+	  	echo "Sorry, only txt files are allowed. ";
+        $uploadOk = 0;
+		}
+	 else {
+		    echo "Your file is txt";
+		    $uploadOk = 1;
+		  }
+}
+
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . "boom.txt")) {
+    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+
+    $output = shell_exec("python txttohandwriting.py");
+    
+    header("Location: download.php");
+
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+?>
